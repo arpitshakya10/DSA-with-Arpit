cddb66a64679bb8602dc5a52f538d007871ed6fd
arpitshakya10 <shakya.arpit10@gmail.com>
2026-06-23 00:56:57 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.23%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(long long n) {
+        if(n<0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        for(int i=2;i<6&&n;i++){
+            while(n%i==0)
+            n=n/i;
+            if(n==1)
+            return true;}
+       
+        return false;
+    }  };
